b1fb6aa0b5655edb2f733129186424b5ec7e2e45
Fahmi Hadi <Kokolopo321@gmail.com>
2022-06-03 11:38:57 +0700

    user domain

---
+++ handler/user_handler.go
@@ -0,0 +1,81 @@
+package handler
+
+import (
+	"net/http"
+
+	"invoiceinaja/auth"
+	"invoiceinaja/domain/user"
+	"invoiceinaja/helper"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UserHandler struct {
+	userService user.IService
+	authService auth.Service
+}
+
+func NewUserHandler(userService user.IService, authService auth.Service) *UserHandler {
+	return &UserHandler{userService, authService}
+}
+
+func (h *UserHandler) UserRegister(c *gin.Context) {
+	var input user.InputRegister
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		res := helper.ApiResponse("Input Data Gagal!1", http.StatusUnprocessableEntity, "gagal", err)
+
+		c.JSON(http.StatusUnprocessableEntity, res)
+		return
+	}
+
+	_, errUser := h.userService.Register(input)
+	if errUser != nil {
+		res := helper.ApiResponse("Input Data Gagal!2", http.StatusBadRequest, "gagal", errUser)
+
+		c.JSON(http.StatusBadRequest, res)
+		return
+	}
+
+	data := gin.H{
+		"status": "Berhasil Membuat Akun Baru!",
+	}
+
+	res := helper.ApiResponse("Berhasil Membuat Akun Baru!", http.StatusCreated, "berhasil", data)
+
+	c.JSON(http.StatusCreated, res)
+}
+
+func (h *UserHandler) Login(c *gin.Context) {
+	var input user.InputLogin
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		res := helper.ApiResponse("Login Failed", http.StatusUnprocessableEntity, "failed", err)
+		c.JSON(http.StatusUnprocessableEntity, res)
+		return
+	}
+
+	loginUser, errLogin := h.userService.Login(input)
+	if errLogin != nil {
+		res := helper.ApiResponse("Login Gagal", http.StatusUnprocessableEntity, "failed", errLogin)
+
+		c.JSON(http.StatusUnprocessableEntity, res)
+		return
+	}
+
+	token, errToken := h.authService.GenerateTokenJWT(loginUser.ID, loginUser.Fullname, loginUser.Role)
+
+	if errToken != nil {
+		res := helper.ApiResponse("Fail Create Token", http.StatusBadRequest, "failed", errToken)
+
+		c.JSON(http.StatusBadRequest, res)
+		return
+	}
+
+	formatter := user.FormatUser(loginUser, token)
+
+	res := helper.ApiResponse("berhasil login", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusCreated, res)
+}
--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"invoiceinaja/auth"
 	"invoiceinaja/config"
 	"invoiceinaja/database"
+	"invoiceinaja/domain/user"
+	"invoiceinaja/handler"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	conf := config.InitConfiguration()
 	database.InitDatabase(conf)
-	//db := database.DB
+	db := database.DB
+
+	userRepo := user.NewUserRepository(db)
+	userService := user.NewUserService(userRepo)
+	authService := auth.NewService()
+	userController := handler.NewUserHandler(userService, authService)
+
+	router := gin.Default()
+	api := router.Group("/api/v1")
+
+	// user
+	api.POST("/users", userController.UserRegister)
+	api.POST("/sessions", userController.Login)
+
+	router.Run()
 
 }
