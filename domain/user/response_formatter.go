299e667d731292a0d48fa1de548611184233927e
Yolanda <tirza15yh@gmail.com>
2022-06-30 14:45:55 +0700

    update response_formatter

--- domain/user/response_formatter.go
+++ domain/user/response_formatter.go
@@ -1,46 +1,46 @@
 package user
 
 type UserFormatter struct {
-	Id           int    `json:"id"`
-	Fullname     string `json:"fullname"`
-	Email        string `json:"email"`
-	NoTlpn       string `json:"phone_number"`
-	BusinessName string `json:"company"`
-	Password     string `json:"password"`
-	Avatar       string `json:"avatar"`
-	Token        string `json:"token"`
+	Id          int    `json:"id"`
+	Fullname    string `json:"fullname"`
+	Email       string `json:"email"`
+	PhoneNumber string `json:"phone_number"`
+	Company     string `json:"company"`
+	Password    string `json:"password"`
+	Avatar      string `json:"avatar"`
+	Token       string `json:"token"`
 }
 
 func FormatUser(user User, token string) UserFormatter {
 	formatter := UserFormatter{
-		Id:           user.ID,
-		Fullname:     user.Fullname,
-		Email:        user.Email,
-		NoTlpn:       user.NoTlpn,
-		BusinessName: user.BusinessName,
-		Password:     user.Password,
-		Avatar:       user.Avatar,
-		Token:        token,
+		Id:          user.ID,
+		Fullname:    user.Fullname,
+		Email:       user.Email,
+		PhoneNumber: user.NoTlpn,
+		Company:     user.BusinessName,
+		Password:    user.Password,
+		Avatar:      user.Avatar,
+		Token:       token,
 	}
 
 	return formatter
 }
 
 type UpdateUserFormatter struct {
-	Id           int    `json:"id"`
-	Fullname     string `json:"nama_lengkap"`
-	Email        string `json:"email"`
-	NoTlpn       string `json:"no_tlpn"`
-	BusinessName string `json:"nama_bisnis"`
+	Id          int    `json:"id"`
+	Fullname    string `json:"fullname"`
+	Email       string `json:"email"`
+	PhoneNumber string `json:"phone_number"`
+	Company     string `json:"company"`
 }
 
 func FormatUpdateUser(user User) UpdateUserFormatter {
 	formatter := UpdateUserFormatter{
-		Id:           user.ID,
-		Fullname:     user.Fullname,
-		Email:        user.Email,
-		NoTlpn:       user.NoTlpn,
-		BusinessName: user.BusinessName,
+		Id:          user.ID,
+		Fullname:    user.Fullname,
+		Email:       user.Email,
+		PhoneNumber: user.NoTlpn,
+		Company:     user.BusinessName,
 	}
 
 	return formatter
