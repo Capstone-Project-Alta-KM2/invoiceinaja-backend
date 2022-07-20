9ad4e1ec3480ba9a81b52447a05d0f5a23e34866
kokolopo <kokolopo321@gmail.com>
2022-07-21 01:55:31 +0700

    add

--- routes/routes.go
+++ routes/routes.go
@@ -20,7 +20,7 @@ func APIRoutes(
 	userService user.IService) {
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "Wellcome to InvoiceinAja New",
+			"message": "Wellcome to InvoiceinAja",
 		})
 	})
 
